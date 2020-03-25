ea273f3acb424448ce6bae55fc214f1b8f9cb3ee
Nikolay Brezin <nikolay.brezin@gmail.com>
2020-03-25 21:00:41 +0300

    initial commit

---
+++ home/klen/.bash_history
@@ -0,0 +1,10 @@
+ls
+cd /
+cd test_gl3.3_glad_glfw_demo/
+make clean
+make -j12
+ls -la
+gears.exe 
+uname -a
+ps
+exit
---
+++ include/isl/stdint.h
@@ -0,0 +1,9 @@
+#ifndef _ISL_INCLUDE_ISL_STDINT_H
+#define _ISL_INCLUDE_ISL_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "isl 0.22.1"
+/* generated using gnu compiler x86_64-kgp-mingw32-gcc (Klen's GNU package (KGP) for target::x86_64-kgp-mingw32 @ host::x86_64-kgp-linux-gnu << TROLLIUS >>) 10.0.1 20200324 (experimental) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
