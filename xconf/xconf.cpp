e1788bc9fd77a36185f6bd6a1597d6c56a55a4ca
Nikolay Brezin <nikolay.brezin@gmail.com>
2020-03-28 14:49:22 +0300

    WIP

--- xconf/xconf.cpp
+++ xconf/xconf.cpp
@@ -1,4 +1,4 @@
-// XConf.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// XConf.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -54,67 +54,6 @@ using namespace gl;
 #include <any>
 #include <functional>
 
-/*
-struct TreeItemBase
-{
-    std::string name;
-    std::string info;
-    std::string type;
-    std::string unit;
-
-    virtual void display() = 0;
-};
-
-template<typename T> struct TreeItem : public TreeItemBase
-{
-    T value;
-    TreeItem(const std::string& value_): value(value_)
-    {
-
-    }
-    void display() override
-    {
-        assert(0 && "not implemented");
-    }
-};
-
-template<> struct TreeItem<float> : public TreeItemBase
-{
-    float value;
-    void display() override
-    {         
-    }
-};
-
-template<> struct TreeItem<uint32_t> : public TreeItemBase
-{
-    uint32_t value;
-    void display() override
-    {         
-    }
-};
-
-class ItemsFactory
-{
-    using TreeItemT = std::unique_ptr<TreeItemBase>;
-public:
-    TreeItemT create(const std::string& type, const std::string& value)
-    {
-        TreeItemT item;
-        if (type == "float")
-        {
-            item = std::make_unique<TreeItem<float>>();
-        }
-        else if (type == "uint32_t")
-        {
-            item = std::make_unique<TreeItem<uint32_t>>();
-        }
-
-
-    }
-};
-*/
-
 static void HelpMarker(const char* desc)
 {
     ImGui::TextDisabled("(?)");
@@ -137,9 +76,12 @@ struct TreeItem
     std::string _info;
     std::string _type;
     std::string _unit;
+
     std::any _value;
     std::any _min_v;
     std::any _max_v;
+    std::any _val_default;
+
     bool _array_flag = false;
     std::vector<TreeItemPtr> _children;
     TreeItem(){}
@@ -170,7 +112,7 @@ struct TreeItem
     {
         if (!_children.empty())
         {
-            if (ImGui::TreeNodeEx(_key.c_str(), ImGuiTreeNodeFlags_DefaultOpen))
+            if (ImGui::TreeNodeEx(_name.c_str(), ImGuiTreeNodeFlags_DefaultOpen))
             {
                 for (const auto& item : _children)
                 {
@@ -182,7 +124,7 @@ struct TreeItem
         else
         {
             ImGui::PushItemWidth(50);
-            ImGui::Text("%-30s:", _key.c_str() );            
+            ImGui::Text("%-30s:", _name.c_str() );            
             ImGui::SameLine();
             ImGui::PushID(_key.c_str());
             if (_type == "uint32_t")
@@ -216,36 +158,45 @@ struct TreeItem
             {
                 std::string tabs_ = tabs + "    ";
                 if (!_name.empty())
-                    fprintf(f, "%s\"name\": \"%s\"\n", tabs_.c_str(), _name.c_str());
-                for (const auto& item : _children)
+                    fprintf(f, "%s\"name\": \"%s\",\n", tabs_.c_str(), _name.c_str());
+                //for (const auto& item : _children)
+                for (int i = 0; i < _children.size(); ++i)
                 {
+                    const auto& item = _children[i];
                     item->serialize(f, tabs_);
+                    if (i != _children.size() - 1)
+                        fprintf(f, ",\n");
+                    else
+                        fprintf(f, "\n");
                 }
             }
-            fprintf(f, "%s%s,\n", tabs.c_str(), _array_flag ? "]" : "}");
+            fprintf(f, "%s%s", tabs.c_str(), _array_flag ? "]" : "}");
         }
         else
         {            
             fprintf(f, "%s{\n", tabs.c_str());
             {
                 std::string tabs_ = tabs + "    ";
-                fprintf(f, "%s\"name\": \"%s\"\n",      tabs_.c_str(), _name.c_str());
-                fprintf(f, "%s\"info\": \"%s\"\n",      tabs_.c_str(), _info.c_str());
-                fprintf(f, "%s\"typeof\": \"%s\"\n",    tabs_.c_str(), _type.c_str());
+                fprintf(f, "%s\"name\": \"%s\",\n",      tabs_.c_str(), _name.c_str());
+                fprintf(f, "%s\"info\": \"%s\",\n",      tabs_.c_str(), _info.c_str());
+                fprintf(f, "%s\"typeof\": \"%s\",\n",    tabs_.c_str(), _type.c_str());
+                fprintf(f, "%s\"unit\": \"%s\",\n",      tabs_.c_str(), _unit.c_str());
                 if (_type == "float")
                 {
-                    fprintf(f, "%s\"val\": \"%f\"\n",       tabs_.c_str(), std::any_cast<float>(_value));
-                    fprintf(f, "%s\"val_min\": \"%f\"\n",   tabs_.c_str(), std::any_cast<float>(_min_v));
-                    fprintf(f, "%s\"val_max\": \"%f\"\n",   tabs_.c_str(), std::any_cast<float>(_max_v));
+                    fprintf(f, "%s\"val\": %f,\n",           tabs_.c_str(), std::any_cast<float>(_value));
+                    fprintf(f, "%s\"val_min\": %f,\n",       tabs_.c_str(), std::any_cast<float>(_min_v));
+                    fprintf(f, "%s\"val_max\": %f,\n",       tabs_.c_str(), std::any_cast<float>(_max_v));
+                    fprintf(f, "%s\"val_default\": %f\n",    tabs_.c_str(), std::any_cast<float>(_val_default));
                 }
                 else if (_type == "uint32_t")
                 {
-                    fprintf(f, "%s\"val\": \"%d\"\n",       tabs_.c_str(), std::any_cast<int>(_value));
-                    fprintf(f, "%s\"val_min\": \"%d\"\n",   tabs_.c_str(), std::any_cast<int>(_min_v));
-                    fprintf(f, "%s\"val_max\": \"%d\"\n",   tabs_.c_str(), std::any_cast<int>(_max_v));
+                    fprintf(f, "%s\"val\": %d,\n",           tabs_.c_str(), std::any_cast<int>(_value));
+                    fprintf(f, "%s\"val_min\": %d,\n",       tabs_.c_str(), std::any_cast<int>(_min_v));
+                    fprintf(f, "%s\"val_max\": %d,\n",       tabs_.c_str(), std::any_cast<int>(_max_v));
+                    fprintf(f, "%s\"val_default\": %d\n",   tabs_.c_str(), std::any_cast<int>(_val_default));
                 }
             }
-            fprintf(f, "%s},\n", tabs.c_str());
+            fprintf(f, "%s}", tabs.c_str());
         }
     }
 };
@@ -257,6 +208,154 @@ static void glfw_error_callback(int error, const char* description)
 
 const char* glsl_version = "#version 130";
 GLFWwindow* window = nullptr;
+
+void init_style2()
+{
+    ImGuiStyle* style = &ImGui::GetStyle();
+    ImVec4* colors = style->Colors;
+
+    style->WindowRounding = 2.0f;             // Radius of window corners rounding. Set to 0.0f to have rectangular windows
+    style->ScrollbarRounding = 3.0f;             // Radius of grab corners rounding for scrollbar
+    style->GrabRounding = 2.0f;             // Radius of grabs corners rounding. Set to 0.0f to have rectangular slider grabs.
+    style->AntiAliasedLines = true;
+    style->AntiAliasedFill = true;
+    style->WindowRounding = 2;
+    style->ChildRounding = 2;
+    style->ScrollbarSize = 16;
+    style->ScrollbarRounding = 3;
+    style->GrabRounding = 2;
+    style->ItemSpacing.x = 10;
+    style->ItemSpacing.y = 4;
+    style->IndentSpacing = 22;
+    style->FramePadding.x = 6;
+    style->FramePadding.y = 4;
+    style->Alpha = 1.0f;
+    style->FrameRounding = 3.0f;
+
+    colors[ImGuiCol_Text] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+    colors[ImGuiCol_TextDisabled] = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
+    colors[ImGuiCol_WindowBg] = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
+    //colors[ImGuiCol_ChildWindowBg]         = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    colors[ImGuiCol_PopupBg] = ImVec4(0.93f, 0.93f, 0.93f, 0.98f);
+    colors[ImGuiCol_Border] = ImVec4(0.71f, 0.71f, 0.71f, 0.08f);
+    colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.04f);
+    colors[ImGuiCol_FrameBg] = ImVec4(0.71f, 0.71f, 0.71f, 0.55f);
+    colors[ImGuiCol_FrameBgHovered] = ImVec4(0.94f, 0.94f, 0.94f, 0.55f);
+    colors[ImGuiCol_FrameBgActive] = ImVec4(0.71f, 0.78f, 0.69f, 0.98f);
+    colors[ImGuiCol_TitleBg] = ImVec4(0.85f, 0.85f, 0.85f, 1.00f);
+    colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.82f, 0.78f, 0.78f, 0.51f);
+    colors[ImGuiCol_TitleBgActive] = ImVec4(0.78f, 0.78f, 0.78f, 1.00f);
+    colors[ImGuiCol_MenuBarBg] = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
+    colors[ImGuiCol_ScrollbarBg] = ImVec4(0.20f, 0.25f, 0.30f, 0.61f);
+    colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.90f, 0.90f, 0.90f, 0.30f);
+    colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.92f, 0.92f, 0.92f, 0.78f);
+    colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+    colors[ImGuiCol_CheckMark] = ImVec4(0.184f, 0.407f, 0.193f, 1.00f);
+    colors[ImGuiCol_SliderGrab] = ImVec4(0.26f, 0.59f, 0.98f, 0.78f);
+    colors[ImGuiCol_SliderGrabActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+    colors[ImGuiCol_Button] = ImVec4(0.71f, 0.78f, 0.69f, 0.40f);
+    colors[ImGuiCol_ButtonHovered] = ImVec4(0.725f, 0.805f, 0.702f, 1.00f);
+    colors[ImGuiCol_ButtonActive] = ImVec4(0.793f, 0.900f, 0.836f, 1.00f);
+    colors[ImGuiCol_Header] = ImVec4(0.71f, 0.78f, 0.69f, 0.31f);
+    colors[ImGuiCol_HeaderHovered] = ImVec4(0.71f, 0.78f, 0.69f, 0.80f);
+    colors[ImGuiCol_HeaderActive] = ImVec4(0.71f, 0.78f, 0.69f, 1.00f);
+    colors[ImGuiCol_Separator] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+    colors[ImGuiCol_SeparatorHovered] = ImVec4(0.14f, 0.44f, 0.80f, 0.78f);
+    colors[ImGuiCol_SeparatorActive] = ImVec4(0.14f, 0.44f, 0.80f, 1.00f);
+    colors[ImGuiCol_ResizeGrip] = ImVec4(1.00f, 1.00f, 1.00f, 0.00f);
+    colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.45f);
+    colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.78f);
+    colors[ImGuiCol_PlotLines] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+    colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+    colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+    colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+    colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+    colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+    colors[ImGuiCol_DragDropTarget] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
+    colors[ImGuiCol_NavHighlight] = colors[ImGuiCol_HeaderHovered];
+    colors[ImGuiCol_NavWindowingHighlight] = ImVec4(0.70f, 0.70f, 0.70f, 0.70f);
+}
+
+void init_style(bool bStyleDark_, float alpha_)
+{
+    ImGuiStyle& style = ImGui::GetStyle();
+
+    // light style from Pacôme Danhiez (user itamago) https://github.com/ocornut/imgui/pull/511#issuecomment-175719267
+    style.Alpha = 1.0f;
+    style.FrameRounding = 3.0f;
+    style.Colors[ImGuiCol_Text] = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+    style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
+    style.Colors[ImGuiCol_WindowBg] = ImVec4(0.94f, 0.94f, 0.94f, 0.94f);
+    style.Colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    style.Colors[ImGuiCol_PopupBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.94f);
+    style.Colors[ImGuiCol_Border] = ImVec4(0.00f, 0.00f, 0.00f, 0.39f);
+    style.Colors[ImGuiCol_BorderShadow] = ImVec4(1.00f, 1.00f, 1.00f, 0.10f);
+    style.Colors[ImGuiCol_FrameBg] = ImVec4(1.00f, 1.00f, 1.00f, 0.94f);
+    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
+    style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+    style.Colors[ImGuiCol_TitleBg] = ImVec4(0.96f, 0.96f, 0.96f, 1.00f);
+    style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(1.00f, 1.00f, 1.00f, 0.51f);
+    style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.82f, 0.82f, 0.82f, 1.00f);
+    style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.86f, 0.86f, 0.86f, 1.00f);
+    style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.98f, 0.98f, 0.98f, 0.53f);
+    style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.69f, 0.69f, 0.69f, 1.00f);
+    style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.59f, 0.59f, 0.59f, 1.00f);
+    style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.49f, 0.49f, 0.49f, 1.00f);
+    style.Colors[ImGuiCol_PopupBg] = ImVec4(0.86f, 0.86f, 0.86f, 0.99f);
+    style.Colors[ImGuiCol_CheckMark] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+    style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.24f, 0.52f, 0.88f, 1.00f);
+    style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+    style.Colors[ImGuiCol_Button] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
+    style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+    style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
+    style.Colors[ImGuiCol_Header] = ImVec4(0.26f, 0.59f, 0.98f, 0.31f);
+    style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
+    style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+    style.Colors[ImGuiCol_ResizeGrip] = ImVec4(1.00f, 1.00f, 1.00f, 0.50f);
+    style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+    style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
+    style.Colors[ImGuiCol_PlotLines] = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+    style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+    style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+    style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+    style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+    style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+
+    if (bStyleDark_)
+    {
+        for (int i = 0; i <= ImGuiCol_COUNT; i++)
+        {
+            ImVec4& col = style.Colors[i];
+            float H, S, V;
+            ImGui::ColorConvertRGBtoHSV(col.x, col.y, col.z, H, S, V);
+
+            if (S < 0.1f)
+            {
+                V = 1.0f - V;
+            }
+            ImGui::ColorConvertHSVtoRGB(H, S, V, col.x, col.y, col.z);
+            if (col.w < 1.00f)
+            {
+                col.w *= alpha_;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i <= ImGuiCol_COUNT; i++)
+        {
+            ImVec4& col = style.Colors[i];
+            if (col.w < 1.00f)
+            {
+                col.x *= alpha_;
+                col.y *= alpha_;
+                col.z *= alpha_;
+                col.w *= alpha_;
+            }
+        }
+    }
+}
 
 int init(GLFWwindow*& window)
 {
@@ -274,7 +373,7 @@ int init(GLFWwindow*& window)
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 
     // Create window with graphics context
-    window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    window = glfwCreateWindow(1280, 1024, "XConf", NULL, NULL);
     if (window == NULL)
         return 1;
     glfwMakeContextCurrent(window);
@@ -301,6 +400,7 @@ int init(GLFWwindow*& window)
         fprintf(stderr, "Failed to initialize OpenGL loader!\n");
         return 1;
     }
+
     return 0;
 }
 
@@ -359,6 +459,12 @@ int my_callback(void* userdata, int type, const char* data, uint32_t length)
         {
             nodes_stack.back()->_type = data;
         }
+        else if (last_key == "unit")
+        {
+            nodes_stack.back()->_unit = data;
+        }
+        else
+            assert(0 && "unsupported record");
     }
         break;
     case JSON_INT:
@@ -382,6 +488,12 @@ int my_callback(void* userdata, int type, const char* data, uint32_t length)
                 else if (node->_type == "uint32_t") set_any((int)   std::atoi(data), node->_max_v);
                 else assert(0);
             }
+            else if (last_key == "val_default")
+            {
+                if      (node->_type == "float")    set_any((float) std::atof(data), node->_val_default);
+                else if (node->_type == "uint32_t") set_any((int)   std::atoi(data), node->_val_default);
+                else assert(0);
+            }
         }
         break;
     case JSON_FLOAT:
@@ -400,6 +512,11 @@ int my_callback(void* userdata, int type, const char* data, uint32_t length)
         else if (last_key == "val_max")
         {
             if (node->_type == "float")    set_any((float)std::atof(data), node->_max_v);
+            else assert(0);
+        }
+        else if (last_key == "val_default")
+        {
+            if (node->_type == "float")    set_any((float)std::atof(data), node->_val_default);
             else assert(0);
         }
     }
@@ -426,7 +543,7 @@ void init_xconf()
         fprintf(stderr, "something wrong happened during init\n");
     }
 
-    std::ifstream input("fadec.json.data");
+    std::ifstream input(".\\data\\fadec.json.data");
     while (input)
     {
         std::string line;
@@ -445,9 +562,13 @@ void init_xconf()
 void render_xconf_window()
 {
     ImGui::Begin("XConf");
+    if (ImGui::Button("Load"))
+    {
+    }
+    ImGui::SameLine();
     if (ImGui::Button("Save"))
     {
-        FILE* f = fopen("out.json", "w");
+        FILE* f = fopen(".\\data\\out.json", "w");
         assert(f);
         root->serialize(f);
         fclose(f);
@@ -476,6 +597,12 @@ int main(int, char**)
     // Setup Platform/Renderer bindings
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
+
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("courier.ttf", 13);
+    ImFont* font = io.Fonts->AddFontFromFileTTF(".\\data\\fonts\\DejaVuLGCSansMono.ttf", 13.0f, NULL, io.Fonts->GetGlyphRangesCyrillic());
+    //init_style(true, 0.5f);
+    init_style2();
+
 
     // Our state
     bool show_demo_window = true;
