dfe52320fba1f476cce1edad7b36fbd5c60bbaee
klenSA <klen_s@mail.ru>
2020-05-08 23:22:35 +0300

    gcc 10.0.1 -> 11.0.0

--- lib/gcc/x86_64-kgp-mingw32/10.0.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../../src/gcc/configure -v --prefix=/opt/x86_64-kgp-mingw32-core2_sse4.1 --target=x86_64-kgp-mingw32 --host=x86_64-kgp-linux-gnu --build=x86_64-kgp-linux-gnu --with-pic --disable-bootstrap --enable-lto --enable-languages=c,c++,lto --enable-plugins --disable-libstdcxx-debug --disable-libstdcxx-pch --enable-nls --enable-static --disable-shared --with-demangler-in-ld --enable-symvers --enable-visibility --enable-libdecbumber --enable-libquadmath --disable-libsanitizer --disable-libssp --disable-libmpx --disable-multilib --enable-decimal-float=yes --enable-version-specific-runtime-libs --disable-libstdcxx-filesystem-ts --with-pkgversion='Klen'\\''s GNU package (KGP) for target::x86_64-kgp-mingw32 @ host::x86_64-kgp-linux-gnu << SELF_ISO >>' CFLAGS='-march=core2 -msse4.1 -pipe -g0 -Ofast -fPIC -fgraphite -ffunction-sections -fdata-sections ' CXXFLAGS='-march=core2 -msse4.1 -pipe -g0 -Ofast -fPIC -fgraphite -ffunction-sections -fdata-sections ' LTCFLAGS= LDFLAGS=";
-static const char thread_model[] = "win32";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
